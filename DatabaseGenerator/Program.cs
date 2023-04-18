8f5e09296b48d4e997d543ac2171fd3b95ebe4de
Timothy Smyth <TimSmyth@allthingshomeless.com>
2023-04-18 13:36:54 -0400

    clean up

--- DatabaseGenerator/Program.cs
+++ DatabaseGenerator/Program.cs
@@ -7,25 +7,15 @@ using System.Text.Json;
 
 
 var url = "https://localhost:7208/quoteHub";
-
 var hubConnection = new HubConnectionBuilder()
                          .WithUrl(url)
                          .Build();
 string jsonString = string.Empty;
 var context = new DatabaseContext();
 
-//hubConnection.On<string>("ReceiveMessage",
-//   message => Console.WriteLine($"SignalR Hub Message: {message}"));
 
 hubConnection.On<string>("ReceiveMessage",
     message => jsonString = message);
-
-//var LastPrice =
-//                JsonSerializer.Deserialize<Price>(jsonString)!;
-
-//Console.WriteLine("key = " + LastPrice.key + " " + "time = " + LastPrice.value.time + " " + "value = " + LastPrice.value.value);
-
-
 try
 {
     await hubConnection.StartAsync();
@@ -33,7 +23,7 @@ try
 
     while (true)
     {
-        //  Console.WriteLine(jsonString);
+
         try
         {
             if (!string.IsNullOrEmpty(jsonString))
@@ -43,21 +33,7 @@ try
                 context.Add<Price>(LastPrice);
                 context.SaveChanges();
             }
-            //    //var message = string.Empty;
 
-            //    //Console.WriteLine("Please specify the action:");
-            //    //Console.WriteLine("0 - broadcast to all");
-            //    //Console.WriteLine("exit - Exit the program");
-
-            //    //var action = Console.ReadLine();
-
-            //    //Console.WriteLine("Please specify the message:");
-            //    //message = Console.ReadLine();
-
-            //    //if (action == "exit")
-            //    //    break;
-
-            //    //  await hubConnection.SendAsync("BroadcastMessage");
         }
         catch (Exception ex)
         {
@@ -76,47 +52,3 @@ catch (Exception ex)
 }
 
 
-//String str;
-//SqlConnection myConn = new SqlConnection("Server=JIMS;Integrated security=SSPI;database=master");
-
-//str = "CREATE DATABASE MyDatabase";
-//SqlCommand myCommand = new SqlCommand(str, myConn);
-//try
-//{
-//    myConn.Open();
-//    myCommand.ExecuteNonQuery();
-//    Console.WriteLine("DataBase is Created Successfully");
-//}
-//catch (System.Exception ex)
-//{
-//    Console.WriteLine("DataBase creation failed");
-//    Console.WriteLine(ex.Message);
-//}
-//finally
-//{
-//    if (myConn.State == ConnectionState.Open)
-//    {
-//        myConn.Close();
-//    }
-//}
-
-//str = "CREATE TABLE Prices\r\n( \r\n   [Key] VARCHAR(2),\r\n   [Time] VARCHAR(2),\r\n   \r\n)";
-// myCommand = new SqlCommand(str, myConn);
-//try
-//{
-//    myConn.Open();
-//    myCommand.ExecuteNonQuery();
-//    Console.WriteLine("Table is Created Successfully");
-//}
-//catch (System.Exception ex)
-//{
-//    Console.WriteLine("Table creation failed");
-//    Console.WriteLine(ex.Message);
-//}
-//finally
-//{
-//    if (myConn.State == ConnectionState.Open)
-//    {
-//        myConn.Close();
-//    }
-//}
